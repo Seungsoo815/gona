e9364fd9d66a1fc97b09d371a097567ab466310b
Kim Seungsoo <yccssk@hanmail.net>
2024-12-12 10:25:10 +0900

    프로젝트 초기구성 생성

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace OperatorPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int c;
+
+            Console.WriteLine("0");
+            string inputStream = Console.ReadLine();
+            int a = int.Parse(inputStream);
+            inputStream = "0";
+            Console.WriteLine("1");
+            inputStream = Console.ReadLine();
+            int b = int.Parse(inputStream);
+
+            while (true)
+            {
+                a = 0;
+                b = 1;
+            }
+
+            c = a + b - a * b;
+
+            Console.WriteLine("{0},{1}", a, b);
+
+ 
+        }
+    }
+}
+
+
